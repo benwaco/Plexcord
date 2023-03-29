c09748230dfce6ba37a6c8aa6cf28d0b556c7c02
Ben Waco <benwaco@outlook.com>
2023-03-28 22:58:40 -0700

    feat: Refactor and improve user status checks in bot.py.
    - Update `ping` and `add` slash commands to respond with ephemeral message
    - Add `status` slash command for users to view their share status
    - Implement functionality to check if a user is archived in `status` command

--- bot.py
+++ bot.py
@@ -41,13 +41,13 @@ async def on_ready():
 # Setup Slash Commands
 @bot.slash_command(guild_ids=[GUILD_ID])
 async def ping(ctx):
-    await ctx.respond(f"Pong! {int(bot.latency * 1000)}ms")
+    await ctx.respond(f"Pong! {int(bot.latency * 1000)}ms", ephemeral=True)
 
 @bot.slash_command(guild_ids=[GUILD_ID])
 async def add(ctx, email: str):
     try:
         plex.myPlexAccount().inviteFriend(email, plex, allowSync=True, allowCameraUpload=True, filterMovies=None, filterTelevision=None, filterMusic=None, allowChannels=None)
-        await ctx.respond(f"Invited {email} to plex server")
+        await ctx.respond(f"Invited {email} to plex server", ephemeral=True)
         # if successful add the email, discord id and share status to the database
         await db_plex["plex"].insert_one({
             "email": email,
@@ -57,7 +57,7 @@ async def add(ctx, email: str):
         })
     except Exception as e:
         print(e)
-        await ctx.respond(f"Error adding {email} to plex server")
+        await ctx.respond(f"Error adding {email} to plex server", ephemeral=True)
 
 # make a slash command to remove a user from plex (based on provided discord id) and change the share status to 'manually removed' in the database
 @bot.slash_command(guild_ids=[GUILD_ID])
@@ -68,7 +68,7 @@ async def remove(ctx, discord_id: str):
         user_data = await db_plex["plex"].find_one({"discord_id": discord_id, "archived": False})
         
         if user_data is None:
-            await ctx.respond(f"User with Discord ID {discord_id} not found in the database or user is archived.")
+            await ctx.respond(f"User with Discord ID {discord_id} not found in the database or user is archived.", ephemeral=True)
             return
         
         email = user_data["email"]
@@ -78,10 +78,29 @@ async def remove(ctx, discord_id: str):
         # update the database
         await db_plex["plex"].update_one({"discord_id": discord_id}, {"$set": {"share_status": "removed manually"}})
         await db_plex["plex"].update_one({"discord_id": discord_id}, {"$set": {"archived": True}})
-        await ctx.respond(f"Removed {email} from plex server")
+        await ctx.respond(f"Removed {email} from plex server", ephemeral=True)
     except Exception as e:
         print(e)
-        await ctx.respond(f"Error removing user from plex server, {e}")
+        await ctx.respond(f"Error removing user from plex server, {e}", ephemeral=True)
+
+# make a basic slash command for customers to view their current share status, print their plex email and share status from the database. tell user if they are archived and explain what archived means
+@bot.slash_command(guild_ids=[GUILD_ID])
+async def status(ctx):
+    try:
+        user_data = await db_plex["plex"].find_one({"discord_id": ctx.author.id})
+        if user_data is None:
+            await ctx.respond(f"You are not in the database.", ephemeral=True)
+            return
+        email = user_data["email"]
+        share_status = user_data["share_status"]
+        archived = user_data["archived"]
+        if archived:
+            await ctx.respond(f"Your email is {email}, and your share status is {share_status}. You are archived. This means that you have been removed from the server manually or we have removed you due to non-payment. If you wish to be re-added, please contact us.", ephemeral=True)
+        else:
+            await ctx.respond(f"Your email is {email}, and your share status is {share_status}.", ephemeral=True)
+    except Exception as e:
+        print(e)
+        await ctx.respond(f"Error retrieving your share status, {e}", ephemeral=True)
 
 
 bot.run(DISCORD_TOKEN)
